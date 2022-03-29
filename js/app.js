20f1f4d082ba6ae2913853afb3c6f1f3f6f7c148
Bharath Ron <bharathgron@gmail.com>
2022-03-29 19:40:37 +0200

    kh

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const navSlide = () => {
 		if (link.style.animation) {
 			link.style.animation = '';
 		} else {
-			link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 +0.2'
+			link.style.animation = 'navLinkFade 2s ease forwards ${index / 7 +0.2'
 		}
 		//{
         //document.getElementById("pid").style.visibility = "hidden";
